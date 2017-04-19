a827f1e87ecdf1926918f0164fc9e5619eb3dfd5
StasKirenkov <stas.kirenkov@mail.ru>
2017-04-20 00:06:57 +0300

    Fixed a typo in ./bin/pre_start.sh

--- src/1.3/bin/pre_start.sh
+++ src/1.3/bin/pre_start.sh
@@ -8,20 +8,20 @@ error_str='';
 
 if [ -z "$FILESYSTEM_BACKUP" ];
 then
-    error_str="$error_str\r'FILESYSTEM_BACKUP' can not be empty. Please set the value to "yes" or "no" in the file $(dirname $(pwd))/name.conf"
+    error_str="$error_str\r'FILESYSTEM_BACKUP' can not be empty. Please set the value to "yes" or "no" in the file $(dirname $(pwd))/main.conf"
 fi;
 
 if [ -z "$MIN_FREE_SPACE" ];
 then
-    error_str="$error_str\r'MIN_FREE_SPACE' can not be empty. Please set the limited value in the file $(dirname $(pwd))/name.conf or set value 0, if you haven't limit"
+    error_str="$error_str\r'MIN_FREE_SPACE' can not be empty. Please set the limited value in the file $(dirname $(pwd))/main.conf or set value 0, if you haven't limit"
 fi;
 
 if [ -z "$MYSQL_BACKUP" ];
 then
-    error_str="$error_str\r'MYSQL_BACKUP' can not be empty. Please set the value to "yes" or "no" in the file $(dirname $(pwd))/name.conf"
+    error_str="$error_str\r'MYSQL_BACKUP' can not be empty. Please set the value to "yes" or "no" in the file $(dirname $(pwd))/main.conf"
 fi;
 
 if [ -z "$ALL_DATA_BASE" ];
 then
-    error_str="$error_str\r'ALL_DATA_BASE' can not be empty. Please set the value to "yes" or "no" in the file $(dirname $(pwd))/name.conf"
+    error_str="$error_str\r'ALL_DATA_BASE' can not be empty. Please set the value to "yes" or "no" in the file $(dirname $(pwd))/main.conf"
 fi;
